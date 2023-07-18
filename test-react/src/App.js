a8958328dd0c41ff160176fb02b675fd4b6f5303
Argan12 <argan.piquet@gmail.com>
2023-07-18 10:03:32 +0200

    [FIX] Error on dockerize db connection. Dockerize react app

--- test-react/src/App.js
+++ test-react/src/App.js
@@ -3,16 +3,24 @@ import './App.css';
 import Home from './Home';
 import { Register } from './Components/Register/Register';
 import { Login } from './Components/Login/Login';
+import { AuthProvider, RequireAuth } from 'react-auth-kit';
+import NewPost from './Components/Posts/NewPost';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <AuthProvider authType = {'localstorage'}
+      authName={'id'}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/new-post" element={
+            <RequireAuth loginPath={'/login'}><NewPost /></RequireAuth>
+          }></Route>
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
